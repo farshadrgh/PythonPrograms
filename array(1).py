edd22b4f9e5c799e5ef107e1274b5d67cf710aaa
parmis <hellboyh886@yahoo.com>
2021-02-03 05:35:13 -0800

    Divided into two & three

---
+++ array(1).py
@@ -0,0 +1,7 @@
+for x in range(0,10):
+    if x % 2 == 0 :
+        print("Divided into Two")
+    elif x % 3 == 0 :
+        print("Divided into Three")
+    else:
+        print("not 2 , not 3 !!!")
