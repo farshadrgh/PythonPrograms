2057ac51cf9750995a504b4af89fd0cf18c2587b
parmis <hellboyh886@yahoo.com>
2021-02-05 23:05:21 -0800

    array(1,10)

--- array(1).py
+++ array(1).py
@@ -1,7 +1,11 @@
-for x in range(0,10):
+list = []
+for x in range(0,100):
     if x % 2 == 0 :
-        print("Divided into Two")
+        "Divided into Twe"
+
     elif x % 3 == 0 :
-        print("Divided into Three")
+        print("Divided into Three", end = " ")
+        print("\t\n")
     else:
-        print("not 2 , not 3 !!!")
+        print("not 2 , not 3 !!!", end = " ")
+        print("\t\n")
