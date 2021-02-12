8156c6d95c36d68615f8c2ddad30f92c06ffedad
parmis <hellboyh886@yahoo.com>
2021-02-12 05:49:58 -0800

    total words

---
+++ how many words.py
@@ -0,0 +1,4 @@
+sentence = "Tutorials point is a learning platform"
+print ("The original string is : " + sentence)
+res = len(sentence.split())
+print ("The number of words in string are : " + str(res))
