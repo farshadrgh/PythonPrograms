d7fa7fd19f1aa0263eb41ec54fea2dbd2b1a556c
parmis <hellboyh886@yahoo.com>
2021-02-21 10:11:56 -0800

    percentage

---
+++ a number with a percentage.py
@@ -0,0 +1,12 @@
+x = 0.2519475658473
+y = -0.751547347
+a = 0.10
+b = -0.50
+print("\nOriginal Number: ", x)
+print("Formatted Number with percentage: "+"{:.2%}".format(x))
+print("Original Number: ", y)
+print("Formatted Number with percentage: "+"{:.2%}".format(y))
+print("Original Number: ", y)
+print("Formatted Number with percentage: "+"{:.2%}".format(a))
+print("Original Number: ", y)
+print("Formatted Number with percentage: "+"{:.2%}".format(b))
