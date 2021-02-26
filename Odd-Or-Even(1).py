c5e9787241adc6a60e3d39016cee8b164854ce12
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-02-25 20:44:40 -0800

    Update Odd-Or-Even(1).py

--- Odd-Or-Even(1).py
+++ Odd-Or-Even(1).py
@@ -1,5 +1,8 @@
-n = int(input("What number are you thinking? "))
-if n % 2 ==0 :
-    print("That's an even number!", "Have another? ")
-else:
-    print("That's an odd number!", "Have another? ")
+def odd_or_even():
+    n = int(input("What number are you thinking? "))
+    if n % 2 ==0 :
+        print("That's an even number!", "Have another? ")
+    else:
+        print("That's an odd number!", "Have another? ")
+        
+print(odd_or_even())
