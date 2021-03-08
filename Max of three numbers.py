827067ace5e6a51c6c674bdbb20a9a748a6f8c16
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-03-08 06:29:44 -0800

    Update Max of three numbers.py

--- Max of three numbers.py
+++ Max of three numbers.py
@@ -1,8 +1,11 @@
 def max_of_three(x ,y ,z):
     if x > y and x > z:
         return x
+    
     elif y > x and y > z:
         return y
+    
     else:
         return z
+    
 print(max_of_three(20, 15 ,35))
