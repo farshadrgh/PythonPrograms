db32e17fd9936f9e7613e62ab99da5f51bc04523
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-03-30 22:48:11 -0700

    Add files via upload

---
+++ RandomChoice.py
@@ -0,0 +1,7 @@
+import random
+
+ColorList = ['red', 'blue', 'green', 'white', 'black']
+SubjectList = ['dog', 'cat', 'tree', 'car', 'house']
+
+print("I saw a ", random.choice(ColorList), random.choice(SubjectList), "in the street")
+print("I saw a ", random.choice(ColorList), random.choice(SubjectList), "in the street, too")
