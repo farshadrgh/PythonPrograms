9a5e84bfa61fdad107b43e7d82dcf83806d0bdfd
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-04 06:16:56 -0700

    Update factorial(n).py

--- factorial(n).py
+++ factorial(n).py
@@ -1,8 +1,9 @@
+n = int(input("What is your number? "))
+
 def factorial(n):
     if n <= 1:
         return 1
     else:
         return n * (factorial(n - 1))
 
-
-print(factorial(4))
+print(factorial(n))
