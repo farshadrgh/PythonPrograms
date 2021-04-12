6a801ea03ebcd814edd725a6f262218878f41226
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-11 21:37:11 -0700

    Update advanced_word_counter.py

--- advanced_word_counter.py
+++ advanced_word_counter.py
@@ -1,6 +1,8 @@
 import collections
 sentence = "As far as the laws of mathematics refer to reality they are not certain as far as they are certain they do not refer to reality"
+
 words = sentence.split()
 word_counts = collections.Counter(words)
+
 for word, count in sorted(word_counts.items()):
-    print('"%s" is repeated %d time%s.' % (word, count, "s" if count > 1 else ""))
+    print("{} is repeated {} time{}.".format(word, count, "s" if count > 1 else ""))
