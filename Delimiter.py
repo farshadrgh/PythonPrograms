0f48fc66a7e5041fdf61dcec9ea641e040947cbd
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-13 07:26:33 -0700

    Add files via upload

---
+++ Delimiter.py
@@ -0,0 +1,5 @@
+# With this program you can delimiter the string
+OurSting = "Most of world's rivers damaged by humans"
+print(OurSting.rsplit(' ', 1))
+print(OurSting.rsplit(' ', 2))
+print(OurSting.rsplit(' ', 5))
