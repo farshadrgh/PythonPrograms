c81abdedd4c93f729fb75d3827df33347660a521
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-16 22:21:33 -0700

    Update array(1).py

--- array(1).py
+++ array(1).py
@@ -1,11 +1,13 @@
-list = []
-for x in range(0,100):
-    if x % 2 == 0 :
-        "Divided into Twe"
+def Array1(a, b)
+    list = []
+    for x in range(a,b+1):
+        if x % 2 == 0 :
+            "Divided into Twe"
 
-    elif x % 3 == 0 :
-        print("Divided into Three", end = " ")
-        print("\t\n")
-    else:
-        print("not 2 , not 3 !!!", end = " ")
-        print("\t\n")
+        elif x % 3 == 0 :
+            print("Divided into Three", end = " ")
+            print("\t\n")
+        else:
+            print("not 2 , not 3 !!!", end = " ")
+            print("\t\n")
+print(Array1(0, 100))
