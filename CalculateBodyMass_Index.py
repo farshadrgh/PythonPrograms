2a517eb2dc7bb0093c4f6a87fcc023925dc779c6
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-20 13:18:36 -0700

    Add files via upload

---
+++ CalculateBodyMass_Index.py
@@ -0,0 +1,3 @@
+Height = float(input("Input your height in Feet: "))
+Weight = float(input("Input your weight in Kilogram: "))
+print("Your body mass index is: ", round(Weight / (Height * Height), 2))
