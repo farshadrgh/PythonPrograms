11a45182049f8535f45bf0b32a11fc768e42d169
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-25 10:56:25 -0700

    Add files via upload

---
+++ VolumeAndArea_Of_Sphere.py
@@ -0,0 +1,6 @@
+pi = 22/7
+Radian = float(input('Radius of sphere: '))
+SurfaceArea = 4 * pi * Radian ** 2
+Volume = (4/3) * (pi * Radian ** 3)
+print("Surface Area is: ", SurfaceArea)
+print("Volume is: ", Volume)
