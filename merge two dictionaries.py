ec1f0a10c0c34814532052123e048bf236cd165b
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-04-25 12:48:40 -0700

    Update merge two dictionaries.py

--- merge two dictionaries.py
+++ merge two dictionaries.py
@@ -2,6 +2,6 @@ d1 = {'a': 100, 'b': 200}
 d2 = {'x': 300, 'y': 200}
 d = d1.copy()
 d.update(d2)
-print(d1)
-print(d2)
-print("Our new dictionary is:", d)
+print("First dictionary:\n", d1)
+print("Second dictionary:\n", d2)
+print("Our new dictionary is:\n", d)
