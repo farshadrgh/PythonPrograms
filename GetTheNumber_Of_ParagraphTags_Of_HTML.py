1523007d9417de65fd340c716e1e42e4c771f25d
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-05-09 18:32:14 -0700

    Add files via upload

---
+++ GetTheNumber_Of_ParagraphTags_Of_HTML.py
@@ -0,0 +1,180 @@
+from bs4 import BeautifulSoup
+html_doc = """
+<!DOCTYPE html>
+
+<html lang="en">
+ <head>
+  <title>Breaking News English  | 2-Page Mini-Lesson | Isolation</title>
+
+  <meta name="description" content="Breaking News English.com - A FREE 2-Page Mini-Lesson on Isolation. Comes with more reading, activities, quizzes and a listening.">
+  <meta name="keywords" content="ESL online activity, Isolation, Breaking News English, 2-Page Mini-Lesson">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="../style-mbd.css" type="text/css" media="all">
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      
+  <!-- meta tag for Facebook Insights -->
+		<meta property="fb:admins" content="BreakingNewsEnglish" />        
+              
+<meta property="og:title" content="Volunteers experience 40 days of isolation in a cave" />
+<meta property="og:description" content="English News Lessons: Free 27-Page lesson plan / 2-page mini-lesson - Isolation - Handouts, online activities, speed reading, dictation, mp3... current events." />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://www.breakingnewsenglish.com/2104/210428-isolation-m.html" /> <meta property="og:image" content="http://www.breakingnewsenglish.com/2104/210428-isolation1.jpg" />
+<meta property="og:site_name" content="www.breakingnewsenglish.com" />
+
+  <!-- Mobile Specific Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body class="page page-help right-sidebar">
+
+<div id="masthead" class="section site-header">
+  <div class="container">
+    <div id="branding" class="content-container match-height">
+
+        <h1 class="site-title"><a href="/" title="Thousands of free news english lessons - 7 levels, 27-page handouts, 2-page mini-lessons, speed reading, 5-speed listening, mp3s &amp; 30+ online activities. Easier / Harder lessons every 2 days. English news lessons cover current events / current affairs topics on World News, Business News, Entertainment, Technology, Science...">Breaking News English</a></h1>
+        
+<h6><a href="../index.html">Home</a>&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;<a href="../help.html">Help This Site</a></h6>
+</div><!-- #branding -->
+
+    <div id="ideas-activities-ebook" class="content-container match-height">
+      <h3 class="h5"><a title="New ideas and printable handouts that save you time - for news English and current affairs / current events lessons." href="../book.html"><span>1,000 Ideas &amp; Activities e-Book</span></a></h3>
+    </div><!-- #ideas-activities-ebook -->
+
+    <div id="more-free-lessons" class="content-container match-height">
+      <h3 class="h5"><a href="https://freeeslmaterials.com/sean_banville_lessons.html" title="Thousands of news English lessons with printable handouts, MP3 listening, online quizzes and more. ESL / EFL lesson plans based on current events / current affairs." target="_blank"><span>Free English Lesson Websites</span></a></h3>
+    </div><!-- #more-free-lessons -->
+
+  </div><!-- .container -->
+</div><!-- header .section -->
+
+
+
+&nbsp;
+
+<div class="advert section">
+  <div class="container">
+    <div class="content-container">
+      <!-- Google Ad -->
+      
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- r-activities -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7356791340648458"
+     data-ad-slot="9132895073"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </div><!-- .content-container -->    
+  </div><!-- .container -->
+</div><!-- .section -->
+
+<div id="main" class="section">
+  <div class="container section-header">
+    <div class="content-container">
+     <header><h3>The 2-page handout</h3></header>
+    </div><!-- .content-container -->
+  </div><!-- .container -->
+
+  <div class="container">
+    
+    <div id="primary" class="content-area match-height">
+      <div class="content-container">          
+
+          <article>
+          
+          <h3>Volunteers experience 40 days of isolation in a cave</h3>
+          
+            <p><a href="210428-isolation-m.pdf" target="_blank"><img src="../images/m.jpg" alt="A 2-Page Mini-Lesson - Isolation" class="responsive"/></a></p>
+            <p><a href="210428-isolation-m.pdf" target="_blank"><strong>Download this mini-lesson</strong></a></p>
+            <p><strong>Try easier levels of this lesson: <a href="210428-isolation-4.html"><font style="background-color: yellow;">Isolation - Level 4</font></a> or <a href="210428-isolation-5.html"><font style="background-color: yellow;">Isolation - Level 5</font></a></strong>.</p>
+
+             <p align="justify"><a href="210428-isolation.pdf" target="_blank"><strong>Download the 27-page lesson</strong></a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<a href="../mini_lessons.html" title="Lots more lessons on topical subjects." target="_blank"><strong>More mini-lessons</strong></a></p>
+            
+            
+            <!-- Sharing -->
+            <div class="sharing">
+              <ul id="social-media-list" class="inline no-bullets">
+                <li id="facebook-like">
+                  <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.breakingnewsenglish.com%2F2104%2F210428-isolation-m.html&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=35" style="border:none; overflow:hidden; height:35px; display: inline; max-width: 100px;"></iframe>
+                </li><!-- #facebook-like -->
+                <li>
+                <a title="... to a friend, student, colleague..." href="javascript:mailpage()">
+                <strong>E-mail this <br>
+                to a friend</strong> <br><br></a>
+              </li>
+              
+              <li>
+                <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-count="none"></a>
+              </li>
+              <li>
+                <a href="https://feeds.feedburner.com/breakingnewsenglish" title="Subscribe to my feed" rel="alternate" type="application/rss+xml"><strong>RSS<br>Feed
+                </strong></a>
+              </li></ul><!-- #social-media-list .no-bullets -->
+            </div>
+          </article>
+          
+<br><hr>
+<p>
+<!-- Google Ad -->
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- r-activities-l -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7356791340648458"
+     data-ad-slot="6039827878"
+     data-ad-format="link"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</p>
+<br><hr>
+
+
+
+<p>The reading</p>
+<p>Could you survive for a few days without your smartphone or without being connected to the Internet? How about living in a cave with no mod cons for 40 days? Fifteen people did just that. They took part in a project called Deep Time to see how they would react to and cope with being totally disconnected from the modern world. Scientists from the Human Adaption Institute lead the $1.9-million project. They wanted to understand how people would adapt to drastic changes in living conditions. They put the volunteers in a cave for nearly six weeks. There was no sunlight and the cave dwellers had no contact with the outside world. They had to rely on their body clock to know when to wake up, go to sleep and eat.</p>
+<br><br>
+
+<p><strong><em>Make sure you try all of the online activities for this reading and listening - There are dictations, multiple choice, drag and drop activities, crosswords, hangman, flash cards, matching activities and a whole lot more. Please enjoy :-)</em></strong></p>
+
+<br><br>
+        
+<p>Most of the volunteers said they enjoyed the experience of total isolation. Two-thirds expressed a desire to remain underground a while longer in order to finish projects they had started. Deep Time's director Christian Clot said the experience would, "test humans' ability to adapt to the loss of their frame of reference for time and space". He said: "Our future as humans on this planet will evolve. We must learn to better understand how our brains are capable of finding new solutions, whatever the situation." One striking observation is that participants lost their sense of time. Mr Clot said: "In our heads, we had walked into the cave 30 days ago". One team member estimated the time underground to be 23 days.</p>
+<p><strong>Sources:</strong><br>
+<ul>
+
+<li>https://www.<strong>abc.net.au</strong>/news/2021-04-25/deep-time-experiment-sees-people-spend-40-days-in-cave/100093966</li>
+<li>https://www.<strong>france24.com</strong>/en/live-news/20210424-french-volunteers-leave-cave-after-40-day-endurance-trial</li>
+<li>https://www.<strong>theguardian.com</strong>/world/2021/apr/25/deep-time-team-ends-40-days-underground-in-french-cave</li>
+
+
+</ul>
+		  </p>
+
+
+
+
+
+
+
+  
+    </div><!-- .content-container -->
+    </div><!-- #primary -->
+    
+    <div id="secondary" class="match-height" role="complementary">
+        <div class="content-container">
+
+         <div class="widget widget-1">
+                   <p>
+
+<!-- Google Ad -->
+                
+"""
+soup = BeautifulSoup(html_doc, 'html.parser')
+print("Number of paragraph tags:")
+print(len(soup.find_all("p")))
