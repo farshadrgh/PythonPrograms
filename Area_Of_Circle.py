e0f86e357fd2b46a5a2b6a8f9f58d68451c65728
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-07-09 08:17:09 +0430

    Update and rename Area of a Circle.py to Area_Of_Circle.py

--- Area_Of_Circle.py
+++ Area_Of_Circle.py
@@ -1,5 +1,7 @@
 from math import *
+
 def circle_area():
   r = float(input ("Input the radius of the circle : "))
   print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
 circle_area()
